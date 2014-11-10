60a8cfc2e9ede2acb6151af668469ad3d8ba1fbd
Henrik Steen <henrist@henrist.net>
2014-11-10 11:52:12 +0100

    Initial files.

---
+++ README.md
@@ -0,0 +1,2 @@
+# BS-app
+Test for en liten app ved bruk av Apache Cordova.
---
+++ fixpath.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# file must be sourced
+# . ./fixpath.sh
+
+export ANDROID_HOME=/henrist/henrik/dev/lib/android-sdk-linux/
+export PATH=$PATH:/henrist/henrik/dev/lib/android-sdk-linux/tools/
